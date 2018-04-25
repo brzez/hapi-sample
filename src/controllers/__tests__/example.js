f197a0d4edde4e33f646d4c109050e6fba088212
brzez <pawel@brzez.com>
2018-04-25 23:15:59 +0200

    sample test

--- src/controllers/__tests__/example.js
+++ src/controllers/__tests__/example.js
@@ -2,9 +2,25 @@
 import "../../../jest/functional";
 import request from 'supertest';
 
-describe('folder', () => {
+describe('example', () => {
   test('list', async () => {
-    const response = await request(global.app.listener).get('/');
+    const response = await request(global.app.listener).get('/example/');
+    expect(response.status).toEqual(200);
+  });
+  test('show', async () => {
+    const response = await request(global.app.listener).get('/example/1');
+    expect(response.status).toEqual(200);
+  });
+  test('create', async () => {
+    const response = await request(global.app.listener).post('/example/').send({});
+    expect(response.status).toEqual(200);
+  });
+  test('update', async () => {
+    const response = await request(global.app.listener).patch('/example/1').send({});
+    expect(response.status).toEqual(200);
+  });
+  test('delete', async () => {
+    const response = await request(global.app.listener).delete('/example/1');
     expect(response.status).toEqual(200);
   });
 });
