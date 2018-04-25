2fb99c7f53a4c67f582ddc68c6e804274aedbbca
brzez <pawel@brzez.com>
2018-04-25 23:15:35 +0200

    cleanup

---
+++ src/boot/index.js
@@ -0,0 +1,20 @@
+// @flow
+import hapi from 'hapi';
+import registerPlugins from '../plugins'
+
+import './dotenv';
+import registerControllers from "./registerControllers";
+
+export default async function boot () {
+  const server = hapi.server({
+    port: process.env.PORT,
+    host: process.env.HOST
+  });
+
+  await registerPlugins(server);
+  await registerControllers(server);
+
+  await server.start();
+  return server;
+}
+
---
+++ src/boot/registerPlugins.js
@@ -0,0 +1,11 @@
+// @flow
+import glob from '../util/glob';
+
+export default async function registerControllers (server) {
+  const controllers = await glob(`${__dirname}/../plugins/{**/index.js,*.js}`);
+
+  for (const controller of controllers) {
+    const required = require(controller).default;
+    await required(server)
+  }
+}
