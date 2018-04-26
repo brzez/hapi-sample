60de6c80782216c563c9eb0657436b80527cd01e
brzez <pawel@brzez.com>
2018-04-26 20:54:43 +0200

    .env.{NODE_ENV}

--- src/boot/dotenv.js
+++ src/boot/dotenv.js
@@ -1,9 +1,18 @@
 // @flow
 import dotenv from 'dotenv';
+import path from 'path';
 
-const path = `${__dirname}/../../.env`;
+const dir = `${__dirname}/../../`;
 
-const result = dotenv.config({path});
+let filename = '.env';
+
+if (process.env.NODE_ENV) {
+  filename = `${filename}.${process.env.NODE_ENV}`;
+}
+
+const result = dotenv.config({
+  path: path.join(dir, filename)
+});
 
 if (result.error) {
   throw result.error;
